d02637c769da40ad4852697ee4a3c7ceb6dbdeee
TranHuy2k2 <giahuytran2002@gmail.com>
2023-01-11 00:57:50 +0700

    finished page loaihang

--- src/views/pages/storage/productCategory/ProductCategory.js
+++ src/views/pages/storage/productCategory/ProductCategory.js
@@ -1,11 +1,15 @@
 import {
     Button,
+    CircularProgress,
     Dialog,
     DialogActions,
     DialogContent,
     DialogContentText,
     DialogTitle,
+    Grid,
     IconButton,
+    Input,
+    Modal,
     Skeleton,
     Table,
     TableBody,
@@ -13,15 +17,41 @@ import {
     TableContainer,
     TableHead,
     TableRow,
-    Tooltip
+    TextField,
+    Tooltip,
+    Typography
 } from '@mui/material';
+import { Box, height, style } from '@mui/system';
 import { IconPencil, IconTrash } from '@tabler/icons';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import productcategoryservice from 'services/productcategory.service';
 import time from 'utils/time';
 
 const { default: MainCard } = require('ui-component/cards/MainCard');
+
+// Styling
+const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    maxWidth: '90%',
+    width: 600,
+    height: 'fit-content',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+    borderRadius: '12px',
+    padding: '16px'
+};
+const inputContainerStyle = {
+    padding: '16px',
+    borderRadius: '10px',
+    marginTop: '8px'
+};
 
 const RowSkeletion = () => (
     <TableRow>
@@ -63,9 +93,101 @@ const DeleteModal = ({ open, onClose, categoryName, onSubmit }) => (
     </Dialog>
 );
 
+const InputLoaiHangModal = ({ index, categories, onClose, onSubmit, refetch }) => {
+    const isUpdating = index !== -1;
+    const [loaiHang, setLoaiHang] = useState({
+        ten: isUpdating ? categories[index].ten : '',
+        gianhap: isUpdating && categories[index].gianhap,
+        giaban: isUpdating && categories[index].giaban
+    });
+    const [loading, setLoading] = useState(false);
+    const isValid = loaiHang.ten && loaiHang.giaban > 0 && loaiHang.gianhap > 0;
+    return (
+        <Modal open={Number.isInteger(index)} onClose={onClose}>
+            <Box sx={modalStyle}>
+                <Typography variant="h2" justifyContent="center" textAlign="center">
+                    {isUpdating ? 'Cập Nhật Loại Hàng' : 'Thêm Loại Hàng Mới'}
+                </Typography>
+                {loading ? (
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            margin: '10px 0'
+                        }}
+                    >
+                        <CircularProgress />
+                    </div>
+                ) : (
+                    <div>
+                        <div style={inputContainerStyle}>
+                            <TextField
+                                sx={{ marginTop: '12px' }}
+                                fullWidth
+                                placeholder="Nhập tên loại hàng"
+                                defaultValue={loaiHang.ten}
+                                label="Tên Loại Hàng"
+                                onChange={(e) => {
+                                    setLoaiHang({
+                                        ...loaiHang,
+                                        ten: e.target.value
+                                    });
+                                }}
+                            />
+                            <TextField
+                                sx={{ marginTop: '12px' }}
+                                fullWidth
+                                type="number"
+                                placeholder="Giá Nhập"
+                                defaultValue={loaiHang.gianhap}
+                                label="Giá Nhập"
+                                onChange={(e) => {
+                                    setLoaiHang({
+                                        ...loaiHang,
+                                        gianhap: +e.target.value
+                                    });
+                                }}
+                            />
+                            <TextField
+                                sx={{ marginTop: '12px' }}
+                                fullWidth
+                                type="number"
+                                placeholder="Giá Bán"
+                                defaultValue={loaiHang.giaban}
+                                label="Giá Bán"
+                                onChange={(e) => {
+                                    setLoaiHang({
+                                        ...loaiHang,
+                                        giaban: +e.target.value
+                                    });
+                                }}
+                            />
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                            <Button
+                                disabled={!isValid}
+                                onClick={async () => {
+                                    if (isValid) {
+                                        setLoading(true);
+                                        await onSubmit(isUpdating, loaiHang, categories[index]?.ma);
+                                        setLoading(false);
+                                        refetch();
+                                    }
+                                }}
+                                variant="contained"
+                            >
+                                Lưu
+                            </Button>
+                        </div>
+                    </div>
+                )}
+            </Box>
+        </Modal>
+    );
+};
+
 const ProductCategory = () => {
-    const [openDeleteModal, setOpenDeleteModal] = useState(false);
-    const [deleCategoryIndex, setDeleteCategoryIndex] = useState(-1);
     const {
         data: categories,
         isLoading,
@@ -74,6 +196,9 @@ const ProductCategory = () => {
         initialData: []
     });
 
+    // DELTE MODAL
+    const [openDeleteModal, setOpenDeleteModal] = useState(false);
+    const [deleCategoryIndex, setDeleteCategoryIndex] = useState(-1);
     const handleOpenDeleteModal = (index) => {
         setOpenDeleteModal(true);
         setDeleteCategoryIndex(index);
@@ -89,6 +214,28 @@ const ProductCategory = () => {
             await productcategoryservice.deleteCategory(categories[deleCategoryIndex].ma);
             refetchAllCategories();
             handleCloseDeleteModal();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // INPUT MODAL
+    const [openInputModal, setOpenInputModal] = useState(null);
+    const handleOpenInputModal = (index) => {
+        if (index >= 0) setOpenInputModal(index);
+        else setOpenInputModal(-1);
+    };
+    const handleCloseInputModal = () => {
+        setOpenInputModal(null);
+    };
+    const handleSubmitInputModal = async (isUpdating, value, ma) => {
+        try {
+            if (isUpdating) {
+                await productcategoryservice.updateCategory(value, ma);
+            } else {
+                await productcategoryservice.addCategory(value);
+            }
+            handleCloseInputModal();
         } catch (error) {
             console.log(error);
         }
@@ -121,7 +268,12 @@ const ProductCategory = () => {
                                     <TableCell>{category.giaban}</TableCell>
                                     <TableCell>{time.toDateandTime(category.updatedAt)}</TableCell>
                                     <TableCell>
-                                        <Tooltip title="Chỉnh sửa">
+                                        <Tooltip
+                                            onClick={() => {
+                                                handleOpenInputModal(index);
+                                            }}
+                                            title="Chỉnh sửa"
+                                        >
                                             <IconButton aria-label="edit">
                                                 <IconPencil color="blue" />
                                             </IconButton>
@@ -144,7 +296,7 @@ const ProductCategory = () => {
                         )}
                     </TableBody>
                 </Table>
-                {openDeleteModal && (
+                {Number.isInteger(openDeleteModal) && (
                     <DeleteModal
                         open={openDeleteModal}
                         onClose={handleCloseDeleteModal}
@@ -152,9 +304,25 @@ const ProductCategory = () => {
                         categoryName={categories[deleCategoryIndex].ten}
                     />
                 )}
+                {Number.isInteger(openInputModal) && (
+                    <InputLoaiHangModal
+                        index={openInputModal}
+                        onClose={handleCloseInputModal}
+                        onSubmit={handleSubmitInputModal}
+                        categories={categories}
+                        refetch={refetchAllCategories}
+                    />
+                )}
             </TableContainer>
             <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '8px' }}>
-                <Button>Thêm Loại Hàng</Button>
+                <Button
+                    variant="contained"
+                    onClick={() => {
+                        handleOpenInputModal();
+                    }}
+                >
+                    Thêm Loại Hàng
+                </Button>
             </div>
         </MainCard>
     );
