de1a4c14b9bc1b8d87f3968ab46a922e09a26c32
TranHuy2k2 <giahuytran2002@gmail.com>
2023-01-18 12:49:36 +0700

    finished khuyenmai

--- src/services/khuyenmaigiam.service.js
+++ src/services/khuyenmaigiam.service.js
@@ -10,6 +10,27 @@ class KhuyenMaiGiam {
             console.log(error);
         }
     }
+    async themKMG(values) {
+        try {
+            await request.post('/khuyenmaigiam', values);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async xoaKMG(ma) {
+        try {
+            await request.delete(`/khuyenmaigiam/${ma}`);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async suaKMG(ma, values) {
+        try {
+            await request.put(`/khuyenmaigiam/${ma}`);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 export default new KhuyenMaiGiam();
--- src/views/pages/khuyenmai/giam/KhuyenMaiGiam.js
+++ src/views/pages/khuyenmai/giam/KhuyenMaiGiam.js
@@ -1,15 +1,56 @@
-import { IconButton } from '@mui/material';
+import {
+    Button,
+    Checkbox,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    FormControlLabel,
+    FormHelperText,
+    IconButton,
+} from '@mui/material';
 import { DataGrid, viVN } from '@mui/x-data-grid';
-import { IconPencil, IconX } from '@tabler/icons';
+import { IconCirclePlus, IconPencil, IconX } from '@tabler/icons';
 import dayjs from 'dayjs';
+import { Formik } from 'formik';
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import khuyenmaigiamService from 'services/khuyenmaigiam.service';
 import MainCard from 'ui-component/cards/MainCard';
+import * as Yup from 'yup';
+import KMGForm from './KMGForm';
+
+const CreateModal = ({ open, onClose, onSubmit }) => {
+    return (
+        <Dialog fullWidth open={open} onClose={onClose}>
+            <DialogTitle>Thêm khuyến mãi giảm</DialogTitle>
+            <DialogContent>
+                <KMGForm onSubmit={onSubmit} onClose={onClose} />
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+const UpdateModal = ({ value, open, onClose, onSubmit }) => {
+    return (
+        <Dialog fullWidth open={open} onClose={onClose}>
+            <DialogTitle>Chỉnh sửa khyến mãi giảm</DialogTitle>
+            <DialogContent>
+                <KMGForm
+                    value={value || {}}
+                    buttonText="Lưu"
+                    onSubmit={onSubmit}
+                    onClose={onClose}
+                />
+            </DialogContent>
+        </Dialog>
+    );
+};
 
 const KhuyenMaiGiam = () => {
     let { data, isLoading, refetch } = useQuery('allKMG', () => khuyenmaigiamService.getAllKMG());
     data = data || [];
-    console.log(data);
     const columns = [
         {
             field: 'ma',
@@ -30,6 +71,7 @@ const KhuyenMaiGiam = () => {
             field: 'tile',
             headerName: 'Tỉ Lệ Giảm (%)',
             flex: 1,
+            renderCell: (params) => `${params.row.tile * 100}%`,
         },
         {
             field: 'updatedAt',
@@ -44,7 +86,13 @@ const KhuyenMaiGiam = () => {
             headerName: '',
             renderCell(params) {
                 return (
-                    <IconButton size="small">
+                    <IconButton
+                        onClick={() => {
+                            console.log(params.row);
+                            handleOpenUpdateModal(params.row);
+                        }}
+                        size="small"
+                    >
                         <IconPencil />
                     </IconButton>
                 );
@@ -55,15 +103,62 @@ const KhuyenMaiGiam = () => {
             headerName: '',
             renderCell(params) {
                 return (
-                    <IconButton onClick={() => {}} size="small">
+                    <IconButton onClick={() => setDeleteId(params.row.ma)} size="small">
                         <IconX />
                     </IconButton>
                 );
             },
         },
     ];
+    const [openCreateModal, setOpenCreateModal] = useState(false);
+    const [updatePayload, setUpdatePayload] = useState(null);
+    const [deleteId, setDeleteId] = useState(null);
+
+    const handleOpenCreateModal = () => setOpenCreateModal(true);
+    const handleCloseCreateModal = () => setOpenCreateModal(false);
+
+    const handleOpenUpdateModal = (value) => setUpdatePayload(value);
+    const handleCloseUpdateModal = () => setUpdatePayload(null);
+
+    const handleCreate = async (values) => {
+        try {
+            await khuyenmaigiamService.themKMG(values);
+        } catch (error) {
+        } finally {
+            refetch();
+            handleCloseCreateModal();
+        }
+    };
+
+    const handleUpdate = async (values) => {
+        try {
+            await khuyenmaigiamService.capnhat(updatePayload.ma, values);
+        } catch (error) {
+        } finally {
+            handleCloseUpdateModal();
+            refetch();
+        }
+    };
+
+    const handleDelete = async () => {
+        try {
+            await khuyenmaigiamService.xoaKMG(deleteId);
+        } catch (error) {
+        } finally {
+            setDeleteId(null);
+            refetch();
+        }
+    };
+
     return (
-        <MainCard title="Các Khuyến Mãi Giảm">
+        <MainCard
+            title="Các Khuyến Mãi Giảm"
+            secondary={
+                <Button startIcon={<IconCirclePlus />} onClick={handleOpenCreateModal}>
+                    Thêm{' '}
+                </Button>
+            }
+        >
             <div style={{ height: '60vh' }}>
                 <DataGrid
                     localeText={viVN.components.MuiDataGrid.defaultProps.localeText}
@@ -72,11 +167,60 @@ const KhuyenMaiGiam = () => {
                     columns={columns}
                     rows={data.map((kmg) => ({
                         id: kmg.ma,
+                        malh: kmg.lh.ma,
                         tenlh: kmg.lh.ten,
                         ...kmg,
                     }))}
                 />
             </div>
+            <CreateModal
+                open={openCreateModal}
+                onClose={handleCloseCreateModal}
+                onSubmit={handleCreate}
+            />
+
+            <UpdateModal
+                open={!!updatePayload}
+                value={updatePayload}
+                onClose={handleCloseUpdateModal}
+                onSubmit={handleUpdate}
+            />
+
+            <Dialog open={deleteId !== null} onClose={() => setDeleteId(null)}>
+                <Formik
+                    initialValues={{
+                        accept: false,
+                    }}
+                    validationSchema={Yup.object().shape({
+                        accept: Yup.boolean().equals([true], 'Vui lòng xác nhận để xóa'),
+                    })}
+                    onSubmit={handleDelete}
+                >
+                    {({ values, errors, handleSubmit, handleChange }) => (
+                        <form onSubmit={handleSubmit}>
+                            <DialogTitle>Xóa khuyến mãi giảm</DialogTitle>
+                            <DialogContent>
+                                <DialogContentText maxWidth={360}>
+                                    Bạn có chắc chắn muốn xóa khuyến mãi giảm này không?
+                                </DialogContentText>
+
+                                <FormControlLabel
+                                    label="Xác nhận xóa khuyến mãi giảm"
+                                    name="accept"
+                                    value={values.accept}
+                                    control={<Checkbox />}
+                                    onChange={handleChange}
+                                />
+
+                                <FormHelperText error>{errors.accept}</FormHelperText>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button type="submit">Xóa</Button>
+                            </DialogActions>
+                        </form>
+                    )}
+                </Formik>
+            </Dialog>
         </MainCard>
     );
 };
