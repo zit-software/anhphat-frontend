87236bc7fe31eeb72ec81d0530557ff4a9ceca57
Kim Minh Thang <thangved02@gmail.com>
2023-08-04 21:17:30 +0700

    fix: tra hang

--- src/themes/compStyleOverride.js
+++ src/themes/compStyleOverride.js
@@ -98,6 +98,7 @@ export default function componentStyleOverrides(theme) {
                         color: theme.darkTextSecondary,
                         fontSize: '0.875rem',
                     },
+                    minWidth: '100px',
                 },
             },
         },
